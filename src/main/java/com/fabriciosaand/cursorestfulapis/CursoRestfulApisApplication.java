623e1070c3901d9888fb42b39dd082019a9e74e5
Fabricio Andrade <fabriciosaand@gmail.com>
2023-09-26 07:01:09 -0300

    fist commit

---
+++ src/main/java/com/fabriciosaand/cursorestfulapis/CursoRestfulApisApplication.java
@@ -0,0 +1,13 @@
+package com.fabriciosaand.cursorestfulapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoRestfulApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoRestfulApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabriciosaand/cursorestfulapis/CursoRestfulApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabriciosaand.cursorestfulapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoRestfulApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
